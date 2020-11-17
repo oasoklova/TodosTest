c32f65f9e48347b15af5a6c768eee41436d72fd3
Olga <oasoklova@yandex.ru>
2020-11-17 18:14:58 +0300

    add docs

--- src/test/java/utils/TestDataGenerator.java
+++ src/test/java/utils/TestDataGenerator.java
@@ -1,6 +1,14 @@
 package utils;
 
+/**
+ * Класс для генерации тестовых данных
+ */
 public class TestDataGenerator{
+    /**
+     * Генерирует строку нужной длины
+     * @param length - Количество символов
+     * @return строка нужной длины
+     */
     public static String generateText(int length){
         StringBuilder builder = new StringBuilder();
         for (int i = 1; i < length; i++) {
